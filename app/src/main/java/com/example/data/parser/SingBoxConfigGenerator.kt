df763570b438225cb2a6923895eb83efb6b56719
tavaamir6-lang <tavaamir6@gmail.com>
2026-09-22 01:34:32 +0330

    Remove unsupported Android strict_route option

--- app/src/main/java/com/example/data/parser/SingBoxConfigGenerator.kt
+++ app/src/main/java/com/example/data/parser/SingBoxConfigGenerator.kt
@@ -14,8 +14,7 @@ object SingBoxConfigGenerator {
             .put("address", JSONArray().put("172.19.0.1/30").put("fdfe:dcba:9876::1/126"))
             .put("mtu", 1500)
             .put("auto_route", true)
-            .put("strict_route", true)
-            .put("dns_mode", "hijack")
+                        .put("dns_mode", "hijack")
             .put("dns_address", JSONArray().put("172.19.0.2").put("fdfe:dcba:9876::2"))
 
         val route = JSONObject()
